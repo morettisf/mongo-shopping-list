7e9ca47cf25cd6ca563d3d7dbf3ac751139f75f5
Paul Moretti <morettisf@gmail.com>
2016-12-12 20:12:16 +0000

    shopping list app with db, tests

---
+++ server.js
@@ -0,0 +1,105 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+var config = require('./config');
+
+var app = express();
+
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+var runServer = function(callback) {
+    mongoose.connect(config.DATABASE_URL, function(err) {
+        if (err && callback) {
+            return callback(err);
+        }
+
+        app.listen(config.PORT, function() {
+            console.log('Listening on localhost:' + config.PORT);
+            if (callback) {
+                callback();
+            }
+        });
+    });
+};
+
+if (require.main === module) {
+    runServer(function(err) {
+        if (err) {
+            console.error(err);
+        }
+    });
+};
+
+
+
+var Item = require('./models/item');
+
+
+app.get('/items', function(req, res) {
+    Item.find(function(err, items) {
+        if (err) {
+            return res.status(500).json({
+                message: 'Internal Server Error'
+            });
+        }
+        res.json(items);
+    });
+});
+
+app.post('/items', function(req, res) {
+    Item.create({
+        name: req.body.name
+    }, function(err, item) {
+        if (err) {
+            return res.status(500).json({
+                message: 'Internal Server Error'
+            });
+        }
+        res.status(201).json(item);
+    });
+});
+
+app.put('/items/:id', function(req, res) {
+    var idParam = req.params.id
+    var newName = req.body.name
+    
+    Item.update( 
+        { _id: idParam }, { $set: { name: newName } },
+        function(err, item) {
+
+            if (err) {
+                if (err.kind === 'ObjectId') {
+                return res.sendStatus(404)
+                }
+            }
+                
+            return res.sendStatus(200)
+            
+        })
+})
+
+app.delete('/items/:id', function(req, res) {
+    var idParam = req.params.id
+    
+    Item.remove({ _id: idParam }, function(err, item) {
+        if (err) {
+            return res.sendStatus(404)
+        }
+        
+        return res.status(200).json(idParam)
+    })
+})
+
+
+app.use('*', function(req, res) {
+    res.status(404).json({
+        message: 'Not Found'
+    });
+});
+
+
+
+exports.app = app;
+exports.runServer = runServer;
---
+++ test/test-server.js
@@ -0,0 +1,174 @@
+global.DATABASE_URL = 'mongodb://localhost/shopping-list-test';
+
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+
+var server = require('../server.js');
+var Item = require('../models/item');
+
+var should = chai.should();
+var app = server.app;
+var itemsData = {}
+chai.use(chaiHttp);
+
+describe('Shopping List', function() {
+    
+    before(function(done) {
+        server.runServer(function() {
+            Item.create({name: 'Broad beans'},
+                        {name: 'Tomatoes'},
+                        {name: 'Peppers'}, function(err, beans, tomatoes, peppers) { //this is the callback function we're going to console log to see ID values
+                        console.log(arguments)
+                        itemsData.beans = beans
+                        itemsData.tomatoes = tomatoes
+                        itemsData.peppers = peppers
+                done();
+            });
+        });
+    });
+    
+    it('get - should list items on get', function(done) {
+        chai.request(app)
+        .get('/items')
+        .end(function(err, res) {
+            res.should.have.status(200);
+            res.should.be.json;
+            res.body.should.be.a('array');
+            res.body.should.have.length(3);
+            res.body[0].should.be.a('object');
+            res.body[0].should.have.property('_id');
+            res.body[0].should.have.property('name');
+            res.body[0]._id.should.be.a('string');
+            res.body[0].name.should.be.a('string');
+            res.body[0].name.should.equal('Broad beans');
+            res.body[1].name.should.equal('Tomatoes');
+            res.body[2].name.should.equal('Peppers');
+            done();
+        });
+    });
+    
+    it('post - should add an item on post', function(done) {
+      chai.request(app)
+        .post('/items')
+        .send({'name': 'Kale'})
+        .end(function(err, res) {
+          should.equal(err, null);
+          res.should.have.status(201);
+          res.should.be.json;
+          res.body.should.be.a('object');
+          res.body.should.have.property('name');
+          res.body.should.have.property('_id');
+          res.body.name.should.be.a('string');
+          res.body._id.should.be.a('string');
+          res.body.name.should.equal('Kale');
+          done();
+        });
+    });
+
+    it('post - if no name in request body', function(done) {
+      chai.request(app)
+        .post('/items')
+        .send({'hats': 2})
+        .end(function(err, res) {
+          res.should.have.status(500);
+          done();
+        });
+    });
+    
+    
+    it('put - should edit an item on put', function(done) {
+      chai.request(app)
+        .put('/items/' + itemsData.beans._id)
+        .send({'name': 'beats', _id: itemsData.beans._id})
+        .end(function(err, res) {
+          res.should.have.status(200);
+          done();
+        })
+    });
+
+
+    it('put - if no item ID exists to edit', function(done) {
+      chai.request(app)
+        .put('/items/15')
+        .send({'name': 'beats', 'id': 15})
+        .end(function(err, res) {
+          res.should.have.status(404);
+          done();
+        })
+    });
+    
+    
+    it('put - if no ID in endpoint', function(done) {
+      chai.request(app)
+        .put('/items')
+        .send({'name': 'beats', 'id': 1})
+        .end(function(err, res) {
+          res.should.have.status(404);
+          done();
+        })
+    });
+    
+    it('put - if no body data exists', function(done) {
+      chai.request(app)
+      .put('/items/1')
+      .send({})
+      .end(function(err, res) {
+        res.should.have.status(404);
+        done();
+      })
+    });
+    
+    it('put - if diffrent ID in endpoint than body', function(done) {
+      chai.request(app)
+      .put('/items/1')
+      .send({'name': 'beats', 'id': 2})
+      .end(function(err, res) {
+        res.should.have.status(404);
+        done();
+      })
+    });
+
+    it('put - if with something other than valid JSON', function(done) {
+      chai.request(app)
+      .put('/items/1')
+      .send('item')
+      .end(function(err, res) {
+        res.should.have.status(404);
+        done();
+      })
+    });
+    
+    it('delete - should delete an item on delete', function(done) {
+      chai.request(app)
+        .delete('/items/' + itemsData.beans._id)
+        .end(function(err, res) {
+          res.should.have.status(200);
+          done();
+        })
+    });
+    
+    it('delete - if no ID exists to delete', function(done) {
+      chai.request(app)
+        .delete('/items/12')
+        .end(function(err, res) {
+          res.should.have.status(404);
+          done();
+        })
+    });
+    
+    it('delete - if no ID exists in endpoint', function(done) {
+      chai.request(app)
+        .delete('/items')
+        .end(function(err, res) {
+          res.should.have.status(404);
+          done();
+        })
+    })
+    
+    after(function(done) {
+        Item.remove(function() {
+            done();
+        });
+    });
+    
+});
